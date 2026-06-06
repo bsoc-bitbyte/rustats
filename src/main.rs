dfd533844a83c342731c22f77a659b91e01d74a6
Om Barkare <ombarkare123@gmail.com>
2026-06-06 16:46:04 +0530

    src/main.rs pass array as slice

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rustats::primary::mean;
 fn main() {
     let values = [1.0, 2.0, 3.0, 4.0];
 
-    match mean(values) {
+    match mean(&values) {
         Some(avg) => println!("mean: {avg}"),
         None => println!("mean: (empty input)"),
     }
